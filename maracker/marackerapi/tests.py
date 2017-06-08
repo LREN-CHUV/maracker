a2cc944d999165667d585365aab1d6bc20adc298
Julien M'Poy <julien.mpoy@gmail.com>
2017-06-08 11:12:52 +0200

    Added a test interrogating the MicroBadger API with wrong values

--- maracker/marackerapi/tests.py
+++ maracker/marackerapi/tests.py
@@ -7,13 +7,18 @@ class MicrobadgerTestCase(TestCase):
         pass
 
     def test_service_can_fetch_data_and_create_model(self):
+        namespace, image_name = "hbpmip", "portal-backend"
         microbadger_data = MicrobadgerService.get_docker_metadata(
-            'hbpmip', 'portal-backend')
-        # print("Retrieved data:")
-        # print(microbadger_data.__dict__)
+            namespace, image_name)
         self.assertIsNotNone(microbadger_data)
+        self.assertEqual(microbadger_data.image_name,
+                         "/".join([namespace, image_name]))
 
     def test_service_handle_non_existent_image(self):
         microbadger_data = MicrobadgerService.get_docker_metadata(
             'toto', 'portal-backend')
+        self.assertIsNone(microbadger_data)
+
+    def test_service_handle_unexpected_values(self):
+        microbadger_data = MicrobadgerService.get_docker_metadata(None, None)
         self.assertIsNone(microbadger_data)
